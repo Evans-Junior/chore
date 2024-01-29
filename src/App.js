661805c48afbd9b2a403ccd3e96b7a0d75323640
Evans kumi <kwakukumi14@gmail.com>
2024-01-29 14:48:58 +0000

    notFound

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./Components/Hero/Hero"
 import './App.css'
 import Header from './Components/Header/Header'; 
 import LandingPage from './Components/LandingPage/LandingPage'; 
+import FileNotFound from './Components/FileNotFound/FileNotFound';
 function App() {
   return (
     <BrowserRouter>
@@ -14,7 +15,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/signup' element={<SignUp />}></Route>
         <Route path='/login' element={<Login />}></Route>
-
+        <Route path='*' element={<FileNotFound/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ isLoggedIn }) {
   return (
     <div>
-    <nav>
-      <div class="nav__logo"><a href="#">Learning</a></div>
-      <ul class="nav__links">
-        <li class="link"><a href="#">Home</a></li>
-        <li class="link"><a href="#">About Us</a></li>
-        <li class="link"><a href="#">Services</a></li>
-        <li class="link"><a href="#">Courses</a></li>
-        <li class="link"><a href="#">Blog</a></li>
-        <li class="link"><a href="#" class="nav__btn">Register</a></li>
-      </ul>
-    </nav>
+      <nav>
+        <div className="nav__logo"><a href="#">Learning</a></div>
+        <ul className="nav__links">
+          <li className="link"><a href="#">Home</a></li>
+          <li className="link"><a href="#">About Us</a></li>
+          <li className="link"><a href="#">Services</a></li>
+          <li className="link"><a href="#">Courses</a></li>
+          <li className="link"><a href="#">Blog</a></li>
+          {isLoggedIn ? (
+            <li className="link"><Link to="/user-profile"><i className="fa fa-user"></i></Link></li>
+          ) : (
+            <li className="link"><Link to="#" className="nav__btn">Register</Link></li>
+          )}
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
